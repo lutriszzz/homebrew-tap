c96a3f0190bf4eb8ee0a87132160c5cd4aee740c
lutriszzz <186792132+lutriszzz@users.noreply.github.com>
2024-10-30 15:16:23 +0800

    Update font-lxgw-wenkai-screen.rb

--- Casks/font/font-lxgw-wenkai-screen.rb
+++ Casks/font/font-lxgw-wenkai-screen.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-wenkai-screen-gb" do
   version "1.501"
-  sha256 ee8e1867e25124e7d7266c671c71316a17f0f1ae87c2841a5a32accca14f90cf
+  sha256 "ee8e1867e25124e7d7266c671c71316a17f0f1ae87c2841a5a32accca14f90cf"
 
   url "https://github.com/lxgw/LxgwWenKai-Screen/releases/download/v#{version}/LXGWWenKaiGBScreen.ttf"
   name "LxgwWenKai Screen"
